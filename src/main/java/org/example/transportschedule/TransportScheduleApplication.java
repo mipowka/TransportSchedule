41ae4c02c2a305548a057917a01a1e3cce126ed1
mipowka <mipowkaa@gmai.com>
2025-02-14 16:42:43 +0300

    Initial commit

---
+++ src/main/java/org/example/transportschedule/TransportScheduleApplication.java
@@ -0,0 +1,13 @@
+package org.example.transportschedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransportScheduleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransportScheduleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/transportschedule/TransportScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.transportschedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransportScheduleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
